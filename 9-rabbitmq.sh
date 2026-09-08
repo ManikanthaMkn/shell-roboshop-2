30949e7d92055e5dc46e42431b3a6993fe0be279
manikantha <manikantha.mkn@gmail.com>
2026-09-08 09:46:30 +0530

    Shell Practice

--- 9-rabbitmq.sh
+++ 9-rabbitmq.sh
@@ -1,41 +1,12 @@
 #!/bin/bash/
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+source ./0-common.sh
+app_name=rabbitmq
 
-mkdir -p $LOGS_FOLDER
-echo "Script Started Executing at:: $(date)" | tee -a $LOG_FILE
-
-#Check whether the user has root privileges or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi #IF I am not root → show error and stop. Otherwise → continue.
+check_root
 
 echo "Please enter the RabbitMQ passowrd to startup" #The password should be "roboshop123"
 read -s RABBITMQ_PASSWD
-
-#Validate the function inputs: exit status and the command used for installation.
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else 
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
 
 cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 VALIDATE $? "Adding rabbitmq repo"
@@ -52,8 +23,4 @@ VALIDATE $? "Starting RabbitMQ"
 rabbitmqctl add_user roboshop $RABBITMQ_PASSWD &>>$LOG_FILE  #roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
 
-
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+print_time
