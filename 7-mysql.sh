2a588c658068012aa9b4750580f81330ae3844aa
manikantha <manikantha.mkn@gmail.com>
2026-09-08 09:04:23 +0530

    Shell Practice

--- 7-mysql.sh
+++ 7-mysql.sh
@@ -1,41 +1,12 @@
 #!/bin/bash/
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-
-mkdir -p $LOGS_FOLDER
-echo "Script Started Executing at:: $(date)" | tee -a $LOG_FILE
-
-#Check whether the user has root privileges or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi #IF I am not root → show error and stop. Otherwise → continue.
+source /.0-common.sh
+app_name=mysql
+check_root
 
 echo "Please enter the root passowrd to srtup"
 read -s MYSQL_ROOT_PASSWORD
 
-#Validate the function inputs: exit status and the command used for installation.
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else 
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
 
 dnf install mysql-server -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL Server"
@@ -49,7 +20,4 @@ VALIDATE $? "Starting MySQL"
 mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD
 VALIDATE $? "Stetting-up root user password"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+print_time
